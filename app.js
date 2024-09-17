0abf694f9d4e5867af3d85a059df793c9a69af3b
danilabonnie <danilagavilan@gmail.com>
2024-09-17 19:12:58 -0300

    aaaa

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require ('express')
 const fs = require ('fs') //permite trabajar con archivos (file system). Viene incluida con node, no se instala
 const app = express();
-const port = 3000
+const port = 4000
 
 
 //middleware
 app.use(express.json()) 
 
-const leeDatos = ()=>{
+
+
+const leerDatos = ()=>{
     try{
     const datos = fs.readFileSync('./data/datos.json')
     return JSON.parse(datos)
@@ -21,34 +23,90 @@ const leeDatos = ()=>{
 
 const escribeDatos = (datos)=>{
     try{
-    fs.writeFileSync('./data/datos.json',json.stringify(datos))//convierte un objeto en js a json
+    fs.writeFileSync('./data/datos.json',JSON.stringify(datos))//convierte un objeto en js a json
 
     }catch(error){
         console.log(error)
     }
 }
+//funcion para reindexar vector
+function reIndexar(datos){
+    let indice =1
+    datos.productos.map((p)=>{
+    p.id = indice;
+    indice++;
+})
+}
+
 
 app.get('/productos', (req,res)=>{
-    res.send('listado de productos')
+    //res.send('listado de productos') 
+    const datos=leerDatos()
+    res.json(datos.productos)
 })
 
 app.post('/productos', (req,res)=>{
-    res.send('cargo listado de productos')
+    const datos=leerDatos()
+    const nuevoProducto={id:datos.productos.length+1,
+        ...req.body
+    }
+    datos.productos.push(nuevoProducto)
+    escribeDatos(datos)
+    res.json({mensaje:'Nuevo Producto Agregado',
+        producto:nuevoProducto
+})
+    // res.send('cargo listado de productos')
 })
 
-app.put('/productos', (req,res)=>{
-    res.send('put')
+app.put('/productos/:id', (req,res)=>{
+    //res.send('put')
+    const id = req.params.id
+    const nuevosDatos = req.body
+    const datos=leerDatos()
+    const prodEncontrado = datos.productos.find((p)=>p.id==req.params.id)
+
+        if(!prodEncontrado){
+          return res.status(404),res.json('No se encuentra el producto')
+        }
+
+        datos.productos = datos.productos.map(p=>p.id==req.params.id?{...p,...nuevosDatos}:p)
+        escribeDatos(datos)
+        res.json({mensaje: 'productos actualizados'})
 })
 
-app.delete('/productos', (req,res)=>{
-    res.send('borrando')
+app.delete('/productos/:id', (req,res)=>{
+    //res.send('borrando')
+    const id = req.params.id
+    const datos=leerDatos()
+    const prodEncontrado = datos.productos.find((p)=>p.id==req.params.id)
+
+        if(!prodEncontrado){
+          return res.status(404),res.json('No se encuentra el producto')
+        }
+
+        datos.productos = datos.productos.filter((p)=>p.id!=req.params.id)
+        reIndexar(datos)
+        escribeDatos(datos)
+        res.json({"Mensaje":'producto eliminado'})
 })
 
-app.get('/productos', (req,res)=>{
-    res.send('productos')
+app.get('/productos/:id', (req,res)=>{
+    // res.send('productos')
+    const datos=leerDatos()
+    const prodEncontrado = datos.productos.find((p)=>p.id==req.params.id)
+
+        if(!prodEncontrado){
+          return res.status(404),res.json('No se encuentra el producto')
+        }
+        else{
+         return res.json({
+            "mensaje": "producto encontrado",
+            "producto": prodEncontrado
+        })
+        }
 })
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log(`servidor corriendo en el puerto ${port}`)
 }
 )
